da6939d032dc18b43b0b0125690a0566a3d764f2
Kusieqs <130232383+Kusieqs@users.noreply.github.com>
2023-12-24 15:39:57 +0100

    New Page to Add Unit

---
+++ LanguageAppWpf/NewUnit.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LanguageAppWpf
+{
+    /// <summary>
+    /// Logika interakcji dla klasy NewUnit.xaml
+    /// </summary>
+    public partial class NewUnit : Window
+    {
+        public NewUnit()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnExit(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAddUnit(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
