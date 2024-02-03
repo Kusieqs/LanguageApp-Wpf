c88cc3af8d6c15577e8e2f4e4c87d3cc2594f3c1
Kusieqs <130232383+Kusieqs@users.noreply.github.com>
2024-02-03 15:14:27 +0100

    CheckedLvl -> method complete, Adding dictionary to set lenght of review

--- LanguageAppWpf/Review.xaml.cs
+++ LanguageAppWpf/Review.xaml.cs
@@ -16,10 +16,19 @@ namespace LanguageAppWpf
 {
     public partial class Review : Window
     {
+        private int wordCount = 0;
+        private bool mistake = false;
         private bool theme = false;
         private bool level = false;
         private bool mode = false;
         List<Word> wordList;
+        Dictionary<string, int> wordDictionary = new Dictionary<string, int>
+        {
+            {"Easy", 15 },
+            {"Medium", 30 },
+            {"Hard", MainWindow.words.Count },
+            {"Mistake", 10}
+        };
         public Review()
         {
             InitializeComponent();
@@ -56,6 +65,42 @@ namespace LanguageAppWpf
             string name = (sender as CheckBox).Name;
             wordList = wordList.Where(x => x.Category.ToString() != name).ToList();
         }
+        private void CheckedLvl(object sender, RoutedEventArgs e)
+        {
+            if(!level)
+            {
+                level = true;
+                StartBtnActive();
+            }
+            else
+            {
+                for(int i = 0; i < 4; i++)
+                {
+                    if (Level.Children.Cast<CheckBox>().First(x => Grid.GetRow(x) == i).IsChecked == true)
+                    {
+                        Level.Children.Cast<CheckBox>().First(x => Grid.GetRow(x) == i).IsChecked = false;
+                        break;
+                    }
+                }
+            }
+            string name = (sender as CheckBox).Name;
+            wordCount = wordDictionary[name];   
+
+            if(name == "Mistake")
+            {
+                mistake = true;
+            }
+            else
+            {
+                mistake = false;
+            }
+        }
+
+        private void UncheckedLvl(object sender, RoutedEventArgs e)
+        {
+
+        }
+
         private void StartBtnActive()
         {
             if(theme && level && mode)
