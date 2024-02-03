ca205e540e9a5dcf3edb621f6aa6d34803c37796
Kusieqs <130232383+Kusieqs@users.noreply.github.com>
2024-02-03 13:49:46 +0100

    Logic of Checkboxes (adding and deleting words from list to review)

--- LanguageAppWpf/Review.xaml.cs
+++ LanguageAppWpf/Review.xaml.cs
@@ -14,23 +14,68 @@ using System.Windows.Shapes;
 
 namespace LanguageAppWpf
 {
-    /// <summary>
-    /// Logika interakcji dla klasy Review.xaml
-    /// </summary>
     public partial class Review : Window
     {
+        private bool theme = false;
+        private bool level = false;
+        private bool mode = false;
+        List<Word> wordList;
         public Review()
         {
             InitializeComponent();
         }
 
-        private void Checked(object sender, RoutedEventArgs e)
+        private void CheckedTheme(object sender, RoutedEventArgs e)
+        {
+            theme = true;
+            StartBtnActive();
+            string name = (sender as CheckBox).Name;
+            wordList = wordList.Union(MainWindow.words.Where(x => x.Category.ToString() == name)).ToList();
+        }
+
+        private void UncheckedTheme(object sender, RoutedEventArgs e)
+        {
+            int row = 0;
+            int column = 1;
+            for(int i = 0; i < 8; i++)
+            {
+                if (Theme.Children.Cast<CheckBox>().First(x => Grid.GetRow(x) == row && Grid.GetColumn(x) == column).IsChecked == false)
+                {
+                    theme = false;
+                    break;
+                }
+                if(i == 3)
+                {
+                    column = 3;
+                    row = 0;
+                    continue;
+                }
+                row++;
+            }
+            StartBtnActive();
+            string name = (sender as CheckBox).Name;
+            wordList = wordList.Where(x => x.Category.ToString() != name).ToList();
+        }
+        private void StartBtnActive()
+        {
+            if(theme && level && mode)
+            {
+                Start.IsEnabled = true;
+            }
+        }
+        private void StartBtn(object sender, RoutedEventArgs e)
         {
 
         }
-        private void Unchecked(object sender, RoutedEventArgs e)
+        private void RestartBtn(object sender, RoutedEventArgs e)
         {
 
         }
+
+        private void StopBtn(object sender, RoutedEventArgs e)
+        {
+
+        }
+
     }
 }
