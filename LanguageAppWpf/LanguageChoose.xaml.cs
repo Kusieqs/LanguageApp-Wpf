21bd60f54b4a81871218b4b61ce73f967916957a
Kusieqs <130232383+Kusieqs@users.noreply.github.com>
2024-02-09 14:30:46 +0100

    Deleting usless usingins

--- LanguageAppWpf/LanguageChoose.xaml.cs
+++ LanguageAppWpf/LanguageChoose.xaml.cs
@@ -1,23 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.IO;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Newtonsoft.Json;
-using System.Data.Common;
-using System.Data.Sql;
 using System.ComponentModel;
-using System.Windows.Threading;
 
 namespace LanguageAppWpf
 {
